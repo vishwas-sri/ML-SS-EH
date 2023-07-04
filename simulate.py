031e6e84696f3dbc33bbe13616d883a6de599679
vishwas-sri <vishwas1058@gmail.com>
2023-07-04 11:07:51 +0530

    Simulation setup

---
+++ simulate.py
@@ -0,0 +1,11 @@
+# Simulation Parameters
+T = 100  # Time span for one slot 100ms
+mu = 0.02   # Sensing duration ratio 
+P1 = 0.5    # Probability of spectrum occupancy
+Pd = 0.9    # Probability of detection
+Pf = 0.1    # Probability of false alarm
+m = 20      # Battery capacity
+Eh = 0.1    # Harvested energy during one slot 
+Pw = -60    # Primary signal power in dBm
+Nw = -70    # Noise power in dBm
+g = 10**-5

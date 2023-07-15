2c69259dc5aab9b051373cbcc7c56f79ee333ecc
vishwas-sri <vishwas1058@gmail.com>
2023-07-15 14:31:36 +0530

    Graph and  transition matrix is corrected now

--- markov.py
+++ markov.py
@@ -18,24 +18,27 @@ Pt1 = 0.5
 m = 25
 
 k = [2,3,5,9]
-delta = [5,10,15,20]
+d = [5,10,15,20]
 
 
 
 
-def calculate_pi(k, delta):
+def calculate_pi(k, d):
 # Create a directed graph
     G = nx.DiGraph()
     
     # Add edges with transition probabilities
     # numbering in the sequence otherwise it 
     # will not give desired result
+    
+    # Add nodes to the graph
+   
     i = 0
     b = 1
-    n = delta*k
+    n = d*k
     p = m*k
     while b == 1:
-        if i <= n:
+        if i <= d*k:
             G.add_edge(i, i, weight=P0)
             G.add_edge(i, k+i, weight=P1)
             i = k+i
@@ -44,14 +47,28 @@ def calculate_pi(k, delta):
             G.add_edge(n, n-1, weight=Ps1)
             G.add_edge(n, n+k-1, weight=Ph1)
             G.add_edge(n, 0, weight=Pt1)
+            
+            
         else:
-            n += 1
-            G.add_edge(n, n-1, weight=Ps1)
-            G.add_edge(n, n+k-1, weight=Ph1)
-            G.add_edge(n, 0, weight=Pt1)
-            k -= 1
+            # n += 1
             if n == p:
+                # G.add_edge(n, n-1, weight=Ps1)
+                # G.add_edge(n, n, weight=Ph1)
+                # G.add_edge(n, 0, weight=Pt1)
                 b=0
+            else:
+                n += 1
+                G.add_edge(n, n-1, weight=Ps1)
+                G.add_edge(n, p, weight=Ph1)
+                G.add_edge(n, 0, weight=Pt1)
+                
+            # n += 1
+            # k -= 1
+            # if n == p:
+            #     G.add_edge(n, n-1, weight=Ps1)
+            #     G.add_edge(n, n, weight=Ph1)
+            #     G.add_edge(n, 0, weight=Pt1)
+            #     b=0
            
     # G.add_edge(i-1, i-2, weight=Ps1)
     # G.add_edge(i-1, i, weight=Ph1)
@@ -85,8 +102,8 @@ def calculate_pi(k, delta):
     # Display the transition matrix
     # print("Transition Matrix:")
     # print(transition_matrix)
-    idx = np.where(transition_matrix[:, 0]==0.5)[0]
-    idx = idx[1]
+    # idx = np.where(transition_matrix[:, 0]==0.5)[0]
+    # idx = idx[1]
     
     values, left = scipy.linalg.eig(transition_matrix, right = False, left = True)
     
@@ -95,10 +112,10 @@ def calculate_pi(k, delta):
     
     pi = left[:,0]
     pi_normalized = [(x/np.sum(pi)).real for x in pi]
-    pi_d = sum(pi_normalized[idx:])
-    return pi_d,transition_matrix
+    pi_d = sum(pi_normalized)
+    return pi_d,transition_matrix,G
     # print("PI_i = ",pi_d)
 
 for j in range(len(k)):
-    for d in range(len(delta)):
-        pi[j,d],tm = calculate_pi(k[d],delta[d])
+    for a in range(len(d)):
+        pi[j,a],tm, G = calculate_pi(k[a],d[a])
